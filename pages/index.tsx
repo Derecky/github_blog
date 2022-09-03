df033c00ed21a622a1cdfb3753a8a10a732ea5ee
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2022-09-03 20:29:08 -0300

    new test title

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Development environment
         </h1>
       </main>
     </div>
