2da77711772227b51d5706cd0f64ebc0cf51b028
xnd-dev <xnd.dev@gmail.com>
2022-09-13 22:14:54 -0300

    feat: creation of global style, theme colors and styled type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../styles/themes/theme'
+import { GlobalStyle } from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
