91fea64a868701a7723d953f3567941ecc082189
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2022-09-21 14:42:09 -0300

    Border removed from the main page

--- src/components/MainPage/styles.ts
+++ src/components/MainPage/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components"
 
 export const MainPageContainer = styled.div `
     display:flex; 
-    justify-content: left; 
-    border:1px solid blue;
+    justify-content: left;
     flex-flow: row wrap;
 `
